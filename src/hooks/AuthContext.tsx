6ec2f0abe891b4fca7a67ace6c32f1286f4d7163
Fischer <fischerrobson@gmail.com>
2021-06-22 13:26:01 -0300

    fim da seguna aula

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from 'react';
+import { useEffect } from 'react';
 import { createContext } from 'react';
 
 import { auth, firebase } from '../services/firebase'
@@ -18,26 +19,49 @@ const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider: React.FC = ({ children }) => {
 
-    const [user, setUser] = useState<User>()
+    const [user, setUser] = useState<User>();
+
+    const getUserInfos = (user: any): void => {
+        console.log(user)
+        const { displayName, photoURL, uid } = user;
+        if (!displayName || !photoURL) {
+            throw new Error("Missing information form Google Account")
+        }
+        setUser({
+            id: uid,
+            name: displayName,
+            avatar: photoURL,
+        })
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+              const { displayName, photoURL, uid } = user
+      
+            if(!displayName || !photoURL) {
+              throw new Error('Missing information from Google Account')
+            }
+      
+            setUser({
+              id: uid,
+              name: displayName,
+              avatar: photoURL
+            })
+            }
+          })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
     async function signInWithGoogle() {
         const provider = new firebase.auth.GoogleAuthProvider();
 
         const result = await auth.signInWithPopup(provider);
 
-
-        if (result.user) {
-            const { displayName, photoURL, uid } = result.user;
-            if (!displayName || !photoURL) {
-                throw new Error("Missing information form Google Account")
-            }
-            setUser({
-                id: uid,
-                name: displayName,
-                avatar: photoURL,
-            })
-        }
-
+        if (result.user) getUserInfos(user);
     }
 
     return (
